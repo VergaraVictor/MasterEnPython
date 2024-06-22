310e1289f02cdf639b4e1e7cc333db9252b672a5
VergaraVictor <68487222+VergaraVictor@users.noreply.github.com>
2024-06-22 11:28:50 -0500

    TiposDeDatosEnPython

---
+++ 02-variables-y-tipos/tipos.py
@@ -0,0 +1,23 @@
+nada = None
+cadena = "Hola soy Victor Vergara WEB"
+cadena = "Desarrollador web"
+entero = 99
+flotante = 4.2
+booleano = False
+lista = [10, 20, 30, 100, 200]
+listaString = [44, "teinta", 30, "Cuarenta"]
+tuplaNoCambia = ("master", "en", "python")
+diccionario = {
+    "nombre": "Victor",
+    "apellido": "Robles",
+    "curso": "Master en Python"
+}
+rango = range(9)
+dato_byte = b"Probando"
+
+# Imprimir la variable
+
+print(dato_byte)
+
+# mostrar tipo de dato
+print(type(dato_byte))

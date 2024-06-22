b2aa211b6a2bf2a9cef3bffe681ee627c63cc07a
VergaraVictor <68487222+VergaraVictor@users.noreply.github.com>
2024-06-21 19:23:05 -0500

    Concatenar

--- 02-variables-y-tipos/variables.py
+++ 02-variables-y-tipos/variables.py
@@ -4,11 +4,13 @@ que dentro guardará un dato, se pueden crear
 muchas variables y que cada una tenga un dato distinto
 """
 
+# Crear variables y asignarles un valor
 texto = "Máster en Python"
 texto2 = "con Víctor Vergara"
 numero = 45
 decimal = 6.7
 
+# Mostrar el valor de las variables
 print(texto)
 print(texto2)
 print(numero)
@@ -16,8 +18,23 @@ print(decimal)
 
 print("----------------------------------------")
 
+# Sustituir el valor de las variables / reasignando valores
 numero = 77
 decimal = 8.9
 
 print(numero)
 print(decimal)
+
+print("-------------------------------------------------------")
+
+# Concatenación
+
+nombre = "Víctor"
+apellidos = "Vergara"
+web = "victorvergaraweb.com"
+
+print(nombre + " " + apellidos + " - " + web)
+print(f"{nombre} {apellidos} - {web}")
+print("Hola me llamo {} {} y mi web es: {}".format(nombre, apellidos, web))
+
+print(nombre, apellidos, web)

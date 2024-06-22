66470a830a30d1a931ddb4b524762f548a2bb62b
VergaraVictor <68487222+VergaraVictor@users.noreply.github.com>
2024-06-21 19:12:05 -0500

    VariablesEnPython

---
+++ 02-variables-y-tipos/variables.py
@@ -0,0 +1,23 @@
+"""
+Una variables es un contenedor de información
+que dentro guardará un dato, se pueden crear
+muchas variables y que cada una tenga un dato distinto
+"""
+
+texto = "Máster en Python"
+texto2 = "con Víctor Vergara"
+numero = 45
+decimal = 6.7
+
+print(texto)
+print(texto2)
+print(numero)
+print(decimal)
+
+print("----------------------------------------")
+
+numero = 77
+decimal = 8.9
+
+print(numero)
+print(decimal)

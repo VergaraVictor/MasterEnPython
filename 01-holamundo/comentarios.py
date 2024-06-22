bb29779c9d860edcd463cb013008b6c6712bd760
VergaraVictor <68487222+VergaraVictor@users.noreply.github.com>
2024-06-21 19:01:53 -0500

    ComentariosDeCodigo

---
+++ 01-holamundo/comentarios.py
@@ -0,0 +1,14 @@
+# Esto me mostrará el texto Hola por la consola
+print("Hola")
+
+# Estas dos instrucciones muestran texto por pantalla
+print("Mundo")
+# print("!!")
+
+"""
+Esto no se mostrara
+    print("Esto no se va a mostrar")
+    print("Porque estará dentro de un comentario multilinea")
+"""
+
+print("Adios!!")

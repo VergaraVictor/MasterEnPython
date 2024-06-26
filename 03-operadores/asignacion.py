3d051629d1624dd13d19546afcdec0429fb29f3b
VergaraVictor <68487222+VergaraVictor@users.noreply.github.com>
2024-06-26 18:52:45 -0500

    Operadores

---
+++ 03-operadores/asignacion.py
@@ -0,0 +1,40 @@
+# Operadores de asignaicion
+
+edad = 55
+
+print(edad)
+
+edad += 5
+edad = edad + 5
+edad -= 5
+print(edad)
+
+
+# Operadores incremento o decremento
+
+year = 2021
+
+# Incremento
+
+year = year + 1
+year += 1
+# Decremento
+year = year - 1
+year -= 1
+
+
+# Pre incremento
+
+year = 1 + year
+
+# Pre decremento
+
+year = 1 - year
+
+print(year)
+
+#Esto no se puede hacer en python
+# year++
+# year--
+# --year
+# ++year

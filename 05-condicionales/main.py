522bb85b1abb25e497506f23a15fd9ebe40711a2
VergaraVictor <68487222+VergaraVictor@users.noreply.github.com>
2024-06-27 18:54:23 -0500

    OperadoresComparacion

--- 05-condicionales/main.py
+++ 05-condicionales/main.py
@@ -12,16 +12,37 @@ if condicion:
 else:
     otras instrucciones
 
+
+# Operadores de comparación
+== igual
+!= diferente
+< menor que
+> mayor que
+<= menor o igual que
+>= mayor o igual que    
+
 """
 
 # Ejemplo 1
 print("############# EJEMPLO 1 ###############################")
 
-#color = "verde"
-color = input("Adivina cual es mi color favorito: ")
+color = "verde"
+#color = input("Adivina cual es mi color favorito: ")
 
 if color == "rojo":
     print("Enhjorabuena!!")
     print("El color es ROJO")
 else:
     print("Color incorrecto !!")
+
+# Ejemplo 2
+print("\n############# EJEMPLO 2 ###############################")
+
+# year = 2020
+year = int(input("En que año estamos?:"))
+
+if year < 2021:
+    print("Estamos antes de 2021 !!")
+else:
+    print("Es una año posterior 2021")
+

eecfa9cb3a88b44ab02d21815868277b53f96230
VergaraVictor <68487222+VergaraVictor@users.noreply.github.com>
2024-06-28 19:11:26 -0500

    EjemploConElBucleWhile
    estructuras de control interactivas

--- 06-bucles/bucle-while.py
+++ 06-bucles/bucle-while.py
@@ -26,3 +26,19 @@ while contador <= 100:
     contador += 1
 
 print(muestrame)
+
+
+print("\n##################EJEMPLO##############")
+numero_usuario = int(input("¿De que número quieres la tabla?: "))
+
+if numero_usuario < 1:
+    numero_usuario = 1
+
+print(f"#### tabla del {numero_usuario} ####")
+
+contador = 1
+while contador <= 10:
+    print(f"{numero_usuario} x {contador} = {numero_usuario*contador}")
+    contador += 1
+else:
+    print("Tabla terminada")

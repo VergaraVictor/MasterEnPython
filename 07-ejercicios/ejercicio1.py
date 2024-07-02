cf3e9471345c70fa892916bb96efe9fee6fd481a
VergaraVictor <68487222+VergaraVictor@users.noreply.github.com>
2024-07-02 18:54:46 -0500

    Ejercicio1

---
+++ 07-ejercicios/ejercicio1.py
@@ -0,0 +1,18 @@
+"""
+Ejercicio1.
+    - Crear variables una "pais" y otra "continente"
+    - Mostrar su valor por pantalla (imprimir)
+    - Poner un comentario diciendo el tipo de dato
+"""
+
+pais = "Colombia"
+Continente = 'America'
+
+print(pais)         #string
+print(Continente)   #string
+year = 2021         #integer
+
+print(f"{pais} - {Continente} - {str(year)}")
+
+print(type(pais))
+print(type(Continente))

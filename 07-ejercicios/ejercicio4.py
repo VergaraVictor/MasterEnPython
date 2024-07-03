fbb9188c670dac2b13ca16aae64012f430a63b6b
VergaraVictor <68487222+VergaraVictor@users.noreply.github.com>
2024-07-02 19:12:20 -0500

    Ejercicio4

---
+++ 07-ejercicios/ejercicio4.py
@@ -0,0 +1,14 @@
+"""
+Ejercicio 4. Pedir dos numeros al usuario y hacer todas las
+operaciones básicas de una calculadora u mostrarlo por pantalla
+"""
+
+numero1 = int(input("Introduce el primer número: "))
+numero2 = int(input("Introduce el segundo número: "))
+
+print("#####CALCULADORA#####")
+print("Suma: " + str(numero1+numero2))
+print("Resta: " + str(numero1-numero2))
+print("Multiplicacion: " + str(numero1*numero2))
+print("Division: " + str(numero1/numero2))
+    

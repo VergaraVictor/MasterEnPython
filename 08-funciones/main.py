7dd8e841f85bb1560e7114ca495775e64327d9eb
VergaraVictor <68487222+VergaraVictor@users.noreply.github.com>
2024-07-04 18:54:43 -0500

    CrearFunciones

---
+++ 08-funciones/main.py
@@ -0,0 +1,31 @@
+"""
+FUNCIONES:
+Una funcion es un conjunto de instrucciones agrupadas bajo
+un nombre concreto que pueden reutilizarse invocando a
+la función tantas veces como sea necesario
+
+def nombreDeMiFuncion(parametros):
+    # BLOQUE DE CODIGO / CONJUNTO DE INSTRUCCIONES
+
+nombreDeMiFuncion(mi_parametro)
+nombreDeMiFuncion(mi_parametro)
+
+"""
+
+# Ejemplo 1
+print("###### EJEMPLO 1 ###########")
+
+# Definir funcion
+def muestraNombre():
+    print("Víctor")
+    print("Paco")
+    print("Juan")
+    print("Francisco")
+    print("Aitor")
+    print("Nestor")
+    print("\n")
+
+# Invocar funciones
+muestraNombre()
+muestraNombre()
+muestraNombre()

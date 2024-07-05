569f73fd5e343d317de84dbea0261bb91bbbdba9
VergaraVictor <68487222+VergaraVictor@users.noreply.github.com>
2024-07-04 19:03:11 -0500

    ParametrosEnFunciones

--- 08-funciones/main.py
+++ 08-funciones/main.py
@@ -29,3 +29,17 @@ def muestraNombre():
 muestraNombre()
 muestraNombre()
 muestraNombre()
+
+# Ejemplo 2: Parametros
+print("###### EJEMPLO 2 ###########")
+
+
+def mostrarTuNombre(nombre, edad):
+    print(f"Tu nombre es: {nombre}")
+
+    if edad >= 18:
+        print("Y eres mayor de edad")
+
+nombre = input("Introduce tu nombre: ")
+edad = int(input("Introduce tu edad: "))
+mostrarTuNombre(nombre, edad)

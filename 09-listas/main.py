80b975ec4130c9e084713097b498946cab0a5c19
VergaraVictor <68487222+VergaraVictor@users.noreply.github.com>
2024-07-09 18:41:11 -0500

    CrearListas

---
+++ 09-listas/main.py
@@ -0,0 +1,19 @@
+"""
+LISTAS(arrays)
+Son coleciones o conjuntos de datos/valores, bajo un unico
+nombre.
+PAra acceder a esos valores podemos usar  un indice númerico.
+"""
+
+pelicula = "Batman"
+
+# Definir lista
+peliculas = ["Batman", "Spiderman", "El señor de los anillos"]
+cantantes = list(('2pac', 'Drake', 'Jennifer Lopez'))
+year = list(range(2020, 2050))
+variada = ["Vitor", 30, 4.4, True, "Texto"]
+
+print(peliculas)
+print(cantantes)
+print(year)
+print(variada)

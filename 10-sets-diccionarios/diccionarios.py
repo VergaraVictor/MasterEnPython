45aeff1c94a9d759ad14978476f894b5ffab4bc6
VergaraVictor <68487222+VergaraVictor@users.noreply.github.com>
2024-07-30 22:05:48 -0500

    Diccionarios

---
+++ 10-sets-diccionarios/diccionarios.py
@@ -0,0 +1,14 @@
+"""
+DICCIONARIO :
+Un tipo de dato que almacena un conjunto de datos.
+En formato clave > valor.
+Es parecido a un array asociativo o un objeto json
+"""
+
+persona = {
+    "nombre": "Víctor",
+    "apellidos": "Vergara",
+    "web": "victorvergara.co"
+}
+
+print(persona["web"])

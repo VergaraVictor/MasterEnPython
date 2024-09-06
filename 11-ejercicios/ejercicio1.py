01226595e82d22433d4c5763dfedec97e269a6cb
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2024-09-06 11:23:41 -0500

    Ejercicio1

--- 11-ejercicios/ejercicio1.py
+++ 11-ejercicios/ejercicio1.py
@@ -3,8 +3,8 @@ Ejercicio 1. Hacer un programa que tenga una lista
 de 8 numeros enteros y haga los siguiente:
 (hecho) - Recorrer la lista y mostrarla 
 (hecho) - Hacer funcion que recorra listas de numeros y devuelva un string "LAs funciones se definen antes de llamarsen"
-- Ordenarla y mostrarla
-- Mostrar su longitud
+(hecho)- Ordenarla y mostrarla
+(hecho)- Mostrar su longitud
 - Buscar algun elemento (que el usuario pida por teclado)
 """
 
@@ -30,3 +30,28 @@ for numero in numeros:
 print(mostrarLista(numeros))
 print(mostrarLista(numeros))
 print(mostrarLista(["Victor", "Juan", "Pepe"]))
+
+# Ordenar y mostrar
+print("##################Ordenar y mostrar##############")
+numeros.sort()
+print(mostrarLista(numeros))
+
+# Mostrar Longitud
+print("##################Mostrar Longitud##############")
+print(len(numeros))
+
+# Busqueda en la lista
+print("##################Busqueda en la lista##############")
+
+busqueda = int(input("Introduce el número: "))
+
+comprobar = isinstance(busqueda, int)
+while not comprobar or busqueda <= 0:
+    busqueda = int(input("Introduce el número: "))
+else:
+    print(f"Has introducido el {busqueda}")
+
+print(f"#### Buscar en la lista el número {busqueda} ####")
+
+search = numeros.index(busqueda)
+print(f"El número buscado en la lista, es el indice: {search}")

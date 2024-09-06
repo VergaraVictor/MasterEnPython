5ef74ea9b278c6eeae9edf443cf06efc566938a4
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2024-09-06 15:12:48 -0500

    ModuloDeFechas

--- 12-modulos/main.py
+++ 12-modulos/main.py
@@ -19,3 +19,22 @@ from mimodulo import *
 
 print(holaMundo("víctor Vergara WEB"))
 print(calculadora(3, 5, True))
+
+
+# Modulo fechas
+import datetime
+
+print(datetime.date.today())
+
+fecha_completa = datetime.datetime.now()
+
+print(fecha_completa)
+print(fecha_completa.year)
+print(fecha_completa.month)
+print(fecha_completa.day)
+
+fecha_personalizada = fecha_completa.strftime("%d/%m/%Y, %H:%M:%S")
+
+print("Mi fecha personalizada", fecha_personalizada)
+
+print(datetime.datetime.now().timestamp())

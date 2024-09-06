4d43df22257516fa56e9fbdd4d54104d5c4d6b4b
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2024-09-06 14:52:05 -0500

    CrearModulos

---
+++ 12-modulos/main.py
@@ -0,0 +1,21 @@
+"""
+Modulos: Son funcionalidades ya hechas para reutilizar.
+
+En python hay muchos modulos, que los puedes consultar aquí:
+https://docs.python.org/es/3/tutorial/modules.html
+
+Podemos conseguir modiles que ya vienen en el lenguaje,
+modulos en internet, y tambien podemos crear nuestros modulos.
+"""
+
+# Importar modulo propio
+
+# import mimodulo
+# from mimodulo import holaMundo
+from mimodulo import *
+
+# print(mimodulo.holaMundo("víctor Vergara WEB"))
+# print(mimodulo.calculadora(3, 5, True))
+
+print(holaMundo("víctor Vergara WEB"))
+print(calculadora(3, 5, True))

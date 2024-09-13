dba31dd9192ed37843cfa31724bbfd075dd3752e
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2024-09-13 15:49:25 -0500

    ManejoDeErroresYExcepciones

---
+++ 15-manejo-errores-excepciones/main.py
@@ -0,0 +1,16 @@
+# Capturar Excepciones y manejar errores en código
+# susceptible a fallos/errores
+
+try:
+    nombre = input("¿Cual es tu nombre?: ")
+
+    if len(nombre) > 1:
+        nombre_usuario = "El nombre es " + nombre
+
+    print(nombre_usuario)
+except:
+    print("Ha ocurrido un error, mete bien el nombre")
+else:
+    print("Todo ha funcionado correctamente")
+finally:
+    print("Fin de la iteración !!")

dbc0835513d4e903afb3f6f965388e05a4977724
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2024-09-13 15:04:34 -0500

    FicheroAll

--- 14-sistema-archivos/ficheros.py
+++ 14-sistema-archivos/ficheros.py
@@ -1,5 +1,6 @@
 from io import open
 import pathlib
+import shutil
 
 # Abrir archivo
 ruta = str(pathlib.Path().absolute()) + "/fichero_texto.txt"
@@ -11,3 +12,26 @@ archivo.write("********Soy un texto metido desde python**********\n")
 
 # Cerrar archivo
 archivo.close()
+
+# Abrir archivo
+ruta = str(pathlib.Path().absolute()) + "/fichero_texto.txt"
+archivo_lectura = open(ruta, "r")
+
+# # Leer contenido
+# contenido = archivo_lectura.read()
+# print(contenido)
+
+# Leer contenido y guardarlo en una lista
+lista = archivo_lectura.readlines()
+archivo_lectura.close()
+
+for frase in lista:
+    # lista_frase = frase.split()
+    # print(lista_frase)
+    print("- "+frase.center(100))
+
+# Copiar
+ruta_original = str(pathlib.Path().absolute()) + "/fichero_texto.txt"
+ruta_nueva = str(pathlib.Path().absolute()) + "/fichero_copiado.txt"
+ruta_alternativa = str(pathlib.Path().absolute()) + "/../07-ejercicios/fichero_copiado88.txt"
+shutil.copyfile(ruta_original, ruta_nueva)

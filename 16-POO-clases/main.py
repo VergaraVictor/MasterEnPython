cfc3226372a9d7edc510d15f329eadd4aefc0b7e
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2024-09-16 09:37:43 -0500

    GettertsAndSetters

--- 16-POO-clases/main.py
+++ 16-POO-clases/main.py
@@ -15,6 +15,17 @@ class Coche:
     plazas = 2
 
     # Métodos, son acciones que hace el objeto (coche) (funciones)
+    def setColor(self, color):
+        self.color = color
+
+    def getColor(self):
+        return self.color
+    
+    def setModelo(self, modelo):
+        self.modelo = modelo
+    
+    def getModelo(self):
+        return self.modelo
 
     def acelerar(self):
         self.velocidad += 1
@@ -30,8 +41,11 @@ class Coche:
 # Crear objetos / Instanciar la clase
 coche = Coche()
 
-print(coche.marca, coche.color)
-print("Velocidad actual: ", coche.velocidad)
+coche.setColor("amarillo")
+coche.setModelo("Murcielago")
+
+print(coche.marca, coche.getModelo(), coche.getColor())
+print("Velocidad actual: ", coche.getVelocidad())
 
 coche.acelerar()
 coche.acelerar()
@@ -39,4 +53,4 @@ coche.acelerar()
 coche.acelerar()
 coche.frenar()
 
-print("Velocidad actual: ", coche.velocidad)
+print("Velocidad actual: ", coche.getVelocidad())

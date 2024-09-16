539ba9ff960cd1f39bc6aa54cdcfb9dc9fe2ef27
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2024-09-16 14:32:03 -0500

    BorrarDatosYActualizarRegistros

--- 19-bases-datos/sqlite.py
+++ 19-bases-datos/sqlite.py
@@ -8,25 +8,47 @@ conexion = sqlite3.connect('pruebas.db')
 cursor = conexion.cursor()
 
 # Crear tabla
-cursor.execute("CREATE TABLE IF NOT EXISTS productos("+
-"id INTEGER PRIMARY KEY AUTOINCREMENT,"+
-"titulo VARCHAR(255), "+
-"descripcion text, "+
-"precio int(255)"+
-")")
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS productos(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    titulo VARCHAR(255), 
+    descripcion text, 
+    precio int(255)
+);
+""")
 
 # Guardar cambios
 conexion.commit()
 
 # Insertar datos
+"""
 cursor.execute("INSERT INTO productos VALUES (NULL, 'Segundo producto', 'descripción de mi producto', 550)")
 conexion.commit()
+"""
+
+# Borrar registros
+cursor.execute("DELETE FROM productos")
+conexion.commit()
+
+# Insertar muchos registros de golpe
+productos = [
+    ("Ordenador portatil", "Buen PC", 700),
+    ("Telefono movil", "Buen Telefono", 140),
+    ("Placa base", "Buena placa", 80),
+    ("Table 15", "Buen Table", 300),
+]
+cursor.executemany("INSERT INTO productos VALUES (null,?,?,?)", productos)
+conexion.commit()
+
+# Update
+cursor.execute("UPDATE productos SET precio=678 WHERE precio=80")
 
 # Listar datos
-cursor.execute("SELECT * FROM productos;")
+cursor.execute("SELECT * FROM productos WHERE precio >= 300;")
 productos = cursor.fetchall()
 
 for producto in productos:
+    print("ID: ", producto[0])
     print("Titulo: ", producto[1])
     print("Descripción: ", producto[2])
     print("Precio: ", producto[3])

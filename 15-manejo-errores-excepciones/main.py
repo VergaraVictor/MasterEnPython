287ae4fefa8fff4d5ee01b6d42fe233904a0391e
VergaraVictor <68487222+VergaraVictor@users.noreply.github.com>
2024-09-15 21:54:11 -0500

    MultiplesErrores

--- 15-manejo-errores-excepciones/main.py
+++ 15-manejo-errores-excepciones/main.py
@@ -1,6 +1,6 @@
 # Capturar Excepciones y manejar errores en código
 # susceptible a fallos/errores
-
+"""
 try:
     nombre = input("¿Cual es tu nombre?: ")
 
@@ -14,3 +14,16 @@ else:
     print("Todo ha funcionado correctamente")
 finally:
     print("Fin de la iteración !!")
+"""
+
+# Multiples Excepciones
+try:
+    numero = int(input("Numero para elevarlo al cuadrado: "))
+    print("El cuadrado es: "+str(numero*numero))
+except TypeError:
+    print("Debes convertir tus cadenas a enteros!!")
+# except ValueError:
+#     print("Introduce Un numero correcto!!")
+except Exception as e:
+    print(type(e))
+    print("Ha ocurrido un error: ", type(e).__name__)

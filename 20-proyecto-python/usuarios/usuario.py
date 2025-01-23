c3c1fa1998326e262f2722f95e283662e56fb0b6
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-01-23 09:51:23 -0500

    ClaseParaLaConexioALaBd

--- 20-proyecto-python/usuarios/usuario.py
+++ 20-proyecto-python/usuarios/usuario.py
@@ -1,16 +1,10 @@
-import mysql.connector
 import datetime
 import hashlib
+import usuarios.conexion as conexion
 
-database = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    passwd="",
-    database="master_python",
-    port=3306
-)
-
-cursor = database.cursor(buffered=True)
+connect = conexion.conectar()
+database = connect[0]
+cursor = connect[1]
 
 class Usuario:
 

8aeaaf81a925b04eae4e9791bf06192eabead1d1
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-01-24 11:32:57 -0500

    CrearUnaClaseParaElPrograma

--- 21-tkinter/01-ventana.py
+++ 21-tkinter/01-ventana.py
@@ -4,32 +4,63 @@
 from tkinter import *
 import os.path
 
-# Crear la ventana raiz
-ventana = Tk()
+class Programa:
 
-# Titulo de la ventana
-ventana.title("Interfaz grafica con Python y víctor Vergara")
+    def __init__(self):
+        self.title = "Master en Python con Victor"
+        self.icon = './imagenes/batman.ico'
+        self.icon_alt = './21-tkinter/imagenes/batman.ico'
+        self.size = "770x470"
+        self.resizable = False
 
-# Comprobar si existe un archivo
-ruta_icono = os.path.abspath('./imagenes/batman.ico')
+    def cargar(self):
 
-if not os.path.isfile(ruta_icono):
-    ruta_icono = os.path.abspath('./21-tkinter/imagenes/batman.ico')
+        # Crear la ventana raiz
+        ventana = Tk()
+        self.ventana = ventana
 
-# Icono de la ventana
-# ventana.iconbitmap("./21-tkinter/imagenes/batman.ico")
-# para que funcione con la ruta absoluta
-ventana.iconbitmap(ruta_icono)
+        # Titulo de la ventana
+        ventana.title(self.title)
 
-# Mostrrar texto en el programa
-texto = Label(ventana, text=ruta_icono)
-texto.pack()
+        # Comprobar si existe un archivo
+        ruta_icono = os.path.abspath(self.icon)
 
-# Cambio en el tamaño de la ventana
-ventana.geometry("750x450")
+        if not os.path.isfile(ruta_icono):
+            ruta_icono = os.path.abspath(self.icon_alt)
 
-# Bloquear ek tamaño de la ventana
-ventana.resizable(0, 0)
+        # Icono de la ventana
+        # ventana.iconbitmap("./21-tkinter/imagenes/batman.ico")
+        # para que funcione con la ruta absoluta
+        ventana.iconbitmap(ruta_icono)
 
-# Arrancar y mostrar la ventana hasta que se cierre
-ventana.mainloop()
+        # Mostrrar texto en el programa
+        texto = Label(ventana, text=ruta_icono)
+        texto.pack()
+
+        # Cambio en el tamaño de la ventana
+        ventana.geometry(self.size)
+
+        # Bloquear ek tamaño de la ventana
+        if self.resizable:
+            ventana.resizable(1, 1)
+        else:
+            ventana.resizable(0, 0)
+    
+    def addTexto(self, dato):
+        texto = Label(self.ventana, text=dato)
+        texto.pack()
+
+    def mostrar(self):
+        # Arrancar y mostrar la ventana hasta que se cierre
+        self.ventana.mainloop()
+
+# Instanciar mi programa
+programa = Programa()
+programa.cargar()
+programa.addTexto("Hola")
+programa.addTexto("Soy un texto")
+programa.addTexto("Bienvenido al master python")
+programa.addTexto("Soy Victor Vergara")
+programa.addTexto("Sigueme en donde quieras :)")
+programa.mostrar()
+

c2547c7209bdad60effaa0c000f671399d248b8f
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-01-24 11:10:28 -0500

    CargarImagenDelProgramaCorrectamente

--- 21-tkinter/01-ventana.py
+++ 21-tkinter/01-ventana.py
@@ -2,6 +2,7 @@
 # Modulo para crear interfaces graficas de usuario
 
 from tkinter import *
+import os.path
 
 # Crear la ventana raiz
 ventana = Tk()
@@ -9,8 +10,20 @@ ventana = Tk()
 # Titulo de la ventana
 ventana.title("Interfaz grafica con Python y víctor Vergara")
 
+# Comprobar si existe un archivo
+ruta_icono = os.path.abspath('./imagenes/batman.ico')
+
+if not os.path.isfile(ruta_icono):
+    ruta_icono = os.path.abspath('./21-tkinter/imagenes/batman.ico')
+
 # Icono de la ventana
-ventana.iconbitmap("./21-tkinter/imagenes/batman.ico")
+# ventana.iconbitmap("./21-tkinter/imagenes/batman.ico")
+# para que funcione con la ruta absoluta
+ventana.iconbitmap(ruta_icono)
+
+# Mostrrar texto en el programa
+texto = Label(ventana, text=ruta_icono)
+texto.pack()
 
 # Cambio en el tamaño de la ventana
 ventana.geometry("750x450")

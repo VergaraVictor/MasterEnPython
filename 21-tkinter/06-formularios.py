e40fb6ca5990eb1ce599aabdc840e892389c0749
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-01-28 16:21:16 -0500

    Botones

--- 21-tkinter/06-formularios.py
+++ 21-tkinter/06-formularios.py
@@ -48,4 +48,12 @@ campo_grande.config(
     pady=15
 )
 
+#Boton
+Label(ventana).grid(row=4, column=1)
+
+boton = Button(ventana, text="Enviar")
+boton.grid(row=5, column=1, sticky=W,)
+boton.config(padx=10, pady=10, bg="green", fg="white")
+
+
 ventana.mainloop()

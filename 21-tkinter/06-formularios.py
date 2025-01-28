1ad126ee2856e93ff38a554cfb29aa35a74656c9
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-01-28 16:12:44 -0500

    OtroCampoDeTexto

--- 21-tkinter/06-formularios.py
+++ 21-tkinter/06-formularios.py
@@ -15,13 +15,22 @@ encabezado.config(
 )
 encabezado.grid(row=0, column=0, columnspan=12, sticky=W)
 
-# Label para el campo
+# Label para el campo Nombre
 label = Label(ventana, text="Nombre")
 label.grid(row=1, column=0, sticky=W, padx=5, pady=5)
 
-# Campo de Texto
+# Campo de Texto nombre
 campo_texto = Entry(ventana)
 campo_texto.grid(row=1, column=1, sticky=W, padx=5, pady=5)
 campo_texto.config(justify="right", state="normal")
+
+# Label para el campo Apellidos
+label = Label(ventana, text="Apellidos")
+label.grid(row=2, column=0, sticky=W, padx=5, pady=5)
+
+# Campo de Texto apellidos
+campo_texto = Entry(ventana)
+campo_texto.grid(row=2, column=1, sticky=W, padx=5, pady=5)
+campo_texto.config(justify="left", state="normal")
 
 ventana.mainloop()

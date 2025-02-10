3ac364b00ee3d407dd689deedb715a1619a51518
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-02-10 07:39:39 -0500

    BotonesQueEjecutanFuncionesConParametros

--- 21-tkinter/08-alertas.py
+++ 21-tkinter/08-alertas.py
@@ -9,13 +9,14 @@ def sacarAlerta():
 
 Button(ventana, text="Mostrar alerta!!!", command=sacarAlerta).pack()
 
-def salir():
+def salir(nombre):
     resultado = MessageBox.askquestion("Salir", "¿Continuar ejecutanfdo la aplicación?")
 
     if resultado != "yes":
+        MessageBox.showinfo("Chao!!", f"Adios {nombre}")
         ventana.destroy()
 
 
-Button(ventana, text="Salir", command=salir).pack()
+Button(ventana, text="Salir", command=lambda: salir("Víctor Vergara")).pack()
 
 ventana.mainloop()

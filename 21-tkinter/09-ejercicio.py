77d4e2322d1468a7d756c7aedbd6b92c00b84d68
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-02-10 07:59:46 -0500

    EjercicioMarcos

--- 21-tkinter/09-ejercicio.py
+++ 21-tkinter/09-ejercicio.py
@@ -10,24 +10,34 @@ from tkinter import messagebox
 
 ventana = Tk()
 ventana.title("Ejercicio completo con Tkinter | Víctor Vergara")
-
+ventana.geometry("404x400")
 ventana.config(bd=25)
 
 numero1 = StringVar()
 numero2 = StringVar()
 resultado = StringVar() 
 
-Label(ventana, text="Primer número: ").pack()
-Entry(ventana, textvariable=numero1).pack()
+marco = Frame(ventana, width=340, height=200)
+marco.config(
+    padx=15,
+    pady=15,
+    bd=5,
+    relief=SOLID  
+)
+marco.pack(side=TOP, anchor=CENTER)
+marco.pack_propagate(False)
 
-Label(ventana, text="Segundo número: ").pack()
-Entry(ventana, textvariable=numero2).pack()
+Label(marco, text="Primer número: ").pack()
+Entry(marco, textvariable=numero1, justify="center").pack()
 
-Label(ventana, text="").pack()
+Label(marco, text="Segundo número: ").pack()
+Entry(marco, textvariable=numero2, justify="center").pack()
 
-Button(ventana, text="Sumar").pack(side="left")
-Button(ventana, text="Restar").pack(side="left")
-Button(ventana, text="Multipicar").pack(side="left")
-Button(ventana, text="Dividir").pack(side="left")
+Label(marco, text="").pack()
+
+Button(marco, text="Sumar").pack(side="left", fill=X, expand=YES)
+Button(marco, text="Restar").pack(side="left", fill=X, expand=YES)
+Button(marco, text="Multipicar").pack(side="left", fill=X, expand=YES)
+Button(marco, text="Dividir").pack(side="left", fill=X, expand=YES)
 
 ventana.mainloop()

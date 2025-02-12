2a5145244c0415bc1400b17da5b096648122c037
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-02-12 07:37:53 -0500

    IntroduccionAlProyectoDeTkinter

---
+++ 21-tkinter/13-proyecto.py
@@ -0,0 +1,12 @@
+"""
+Crear un programa que tenga:
+- ventana
+- Tamaño fijo
+- No redimensionable
+- un Menu
+- Diferenttes Pantallas
+- Formulario de añadir Productos
+- Guardar datos Temporalmente
+- Mostrat datos listados en la pantalla home
+- Opcion de salir
+"""

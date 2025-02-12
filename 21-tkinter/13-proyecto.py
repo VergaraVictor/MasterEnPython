3ddc5bcad50b34ba2ff83ede1af5ffb0d7d56c25
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-02-12 07:47:01 -0500

    MenusDelPrograma

--- 21-tkinter/13-proyecto.py
+++ 21-tkinter/13-proyecto.py
@@ -3,20 +3,32 @@ Crear un programa que tenga:
 (hecho) - ventana
 (hecho) - Tamaño fijo
 (hecho) - No redimensionable
-- un Menu
+(hecho) - un Menu (Inicio, Añadir, Información, Salir)
+(hecho) - Opcion de salir
 - Diferenttes Pantallas
 - Formulario de añadir Productos
 - Guardar datos Temporalmente
 - Mostrat datos listados en la pantalla home
-- Opcion de salir
+
 """
 
 from tkinter import *
 
-
+# Definir Ventana
 ventana = Tk()
 ventana.geometry("500x500")
 ventana.title("Proyecto Tkinter - Master en Python")
 ventana.resizable(0, 0)
 
+#Menú superior
+menu_superior = Menu(ventana)
+menu_superior.add_command(label="Inicio")
+menu_superior.add_command(label="Añadir")
+menu_superior.add_command(label="información")
+menu_superior.add_command(label="Salir", command=ventana.quit)
+
+# Cargar Menu
+ventana.config(menu=menu_superior)
+
+# Cargar Ventana
 ventana.mainloop()

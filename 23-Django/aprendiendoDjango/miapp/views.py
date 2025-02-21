1756bd04a4d94cc41c1e8c50515abab44537e14b
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-02-21 07:50:22 -0500

    CrearVistasYRutas

--- 23-Django/aprendiendoDjango/miapp/views.py
+++ 23-Django/aprendiendoDjango/miapp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+# MVC = Modelo Vista Controlador -> Acciones (metodos)
+# MVT = Modelo Template Vista -> Acciones (metodos)
+
+def hola_mundo(request):
+    return HttpResponse("""
+        <h1>hola mundo con Django!!</h1>
+        <h3>Soy Víctor Vergara Web</h3>    
+    """)

638486b3a661a154b8379ac44cf1b093d949422e
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-02-24 07:50:20 -0500

    ParametrosOpcionalesEnLasRutas

--- 23-Django/aprendiendoDjango/miapp/views.py
+++ 23-Django/aprendiendoDjango/miapp/views.py
@@ -52,5 +52,10 @@ def pagina(request):
         <p>Creado por víctor Robles</p>
     """)
 
-def contacto(request, nombre, apellidos):
-    return HttpResponse(layout+f"<h2>Contacto {nombre} {apellidos}</h2>")
+def contacto(request, nombre="", apellidos=""):
+    html = "" 
+    if nombre and apellidos:
+        html += "<p>El nombre completo es:</p>"
+        html += f"<h3>{nombre} {apellidos}</h3>"
+
+    return HttpResponse(layout+f"<h2>Contacto</h2>"+html)

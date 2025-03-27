5babba21457511c89cec22db398458efa928ce40
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-03-27 07:43:32 -0500

    PersonalizarFormularios

--- 23-Django/aprendiendoDjango/miapp/forms.py
+++ 23-Django/aprendiendoDjango/miapp/forms.py
@@ -3,13 +3,26 @@ from django import forms
 class FormArticle(forms.Form):
 
     title = forms.CharField(
-        label= "Titulo"
+        label= "Titulo",
+        max_length=20,
+        required=False,
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'Mete el titulo',
+                'class': 'titulo_form_article'
+            }
+        )
     )
 
     content = forms.CharField(
         label = "Contenido",
         widget=forms.Textarea
     )
+    content.widget.attrs.update({
+        'placeholder': 'Mete el Contenido YAAA',
+        'class': 'contenido_form_article',
+        'id': 'contenido_form'
+    })
 
 
     public_options = [

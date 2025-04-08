c190b5ba600178837fd9c9d6730aeb7e187fcf41
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-04-08 07:48:03 -0500

    CrearVistaYUrlParaLasPaginas

--- 23-Django/ProyectoDjango/ProyectoDjango/urls.py
+++ 23-Django/ProyectoDjango/ProyectoDjango/urls.py
@@ -17,10 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from mainapp import views
+# from pages import views as page_views
+import pages.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
     path('inicio/', views.index, name="inicio"),
-    path('sobre-nosotros/', views.about, name="about")
+    # path('pagina/', page_views.page, name="page")
+    path('pagina/', pages.views.page, name="page")
+
 ]

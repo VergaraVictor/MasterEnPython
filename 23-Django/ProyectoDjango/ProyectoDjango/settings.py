ac51a923993f7f1d51bbc67305b68e3e4a44a6ba
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-04-10 07:28:57 -0500

    ConfiguracionDeleditorDeTextoEnriquecido

--- 23-Django/ProyectoDjango/ProyectoDjango/settings.py
+++ 23-Django/ProyectoDjango/ProyectoDjango/settings.py
@@ -124,3 +124,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Ckeditor
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': 'Custom',
+        'toolbar_Custom': [
+            ['Bold', 'Italic', 'Underline'],
+            ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
+            ['Link', 'Unlink'],
+            ['RemoveFormat', 'Source', 'Table', 'Image']
+        ]
+    }
+}

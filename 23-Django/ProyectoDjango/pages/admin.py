7c3c8c40907386372987baeeae77c6ecbad360a1
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-04-21 07:46:06 -0500

    PersonalizarListadosParte1

--- 23-Django/ProyectoDjango/pages/admin.py
+++ 23-Django/ProyectoDjango/pages/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from .models import Page
 
+# configuración extra para mi modelo
+class PageAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'update_at')
+    search_fields = ('title', 'content')
+    list_filter = ('visible',)
+    list_display = ('title', 'visible', 'created_at')
+    ordering = ('-created_at',)
+
 # Register your models here.
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
 
 # Configuracion del Panel
 title = "Proyecto con"

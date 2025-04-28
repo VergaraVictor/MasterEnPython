073c2340a535f22af21ad7011bcc6631c0300884
Victor Vergara <68487222+VergaraVictor@users.noreply.github.com>
2025-04-28 07:45:05 -0500

    CrearRutas

--- 24-Flask/main.py
+++ 24-Flask/main.py
@@ -7,5 +7,17 @@ app = Flask(__name__)
 def index():
     return "Aprendiendo Flask con Víctor Vergara"
 
+@app.route('/informacion')
+def informacion():
+    return "<h1>Página de información</h1>"
+
+@app.route('/contacto')
+def contacto():
+    return "<h1>Página de contacto</h1>"
+
+@app.route('/lenguajes-de-programacion')
+def lenguajes():
+    return "<h1>Página de lenguajes</h1>"
+
 if __name__ == '__main__':
     app.run(debug=True)
